d9a8be97ed19686329114e0310f69a385a265051
Akos Herko <herkoak@gmail.com>
2022-03-16 20:16:45 +0100

    Initial commit with Spring Starter

---
+++ src/main/java/com/herko/springbootstartersimplejwt/SimpleJwtSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.herko.springbootstartersimplejwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleJwtSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleJwtSpringBootStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/herko/springbootstartersimplejwt/SimpleJwtSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herko.springbootstartersimplejwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleJwtSpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
